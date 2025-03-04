60260b14fc5c7302984b14dc374197cefc459fdb
Felipe Akio Nishimura <felipe.akio.nishimura@gmail.com>
2025-03-04 04:17:04 -0300

    vim

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -eo pipefail
+
+has() {
+  [[ -x "$(command -v "$1")" ]];
+}
+
+has_not() {
+  ! has "$1" ;
+}
+
+ok() {
+  echo "→ "$1" OK"
+}
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+    git git-core \
+    vim \
+    curl 
+
+ok "System updated!"
+ok "Git"
+ok "Vim"
+ok "curl"
+
+if has_not pip; then
+  sudo apt-get install python-pip python-dev build-essential
+  sudo pip install --upgrade pip
+  sudo pip install --upgrade virtualenv
+fi
+ok "GCC"
+
+if ! [[ -d "$HOME/.nvm" ]]; then
+  NODE_VERSION=4
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+  source ~/.bashrc
+  nvm install $NODE_VERSION
+  nvm use $NODE_VERSION
+  nvm alias default $NODE_VERSION
+fi
+ok "NVM"
+
+npm i -g nodemon hmh-cli clima
+ok "NodeJS global modules"
+
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+ok "Java"
